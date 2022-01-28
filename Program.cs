597e669e5f7c3e3598ac1abc08f57085f2abe704
petrovszkid <petrovszkid.12d@acsjszki.hu>
2022-01-28 12:53:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_28_Opening_weekend
+{
+    class OpeningWeekend
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
